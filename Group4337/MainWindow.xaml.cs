433145e69f1ef73814438d610f545e799074dde1
Dilyara <di.su.re.07@gmail.com>
2026-03-06 16:27:10 +0300

    Добавила обработчик событий нажатия кнопки "Импорт данных".

--- Group4337/MainWindow.xaml.cs
+++ Group4337/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Group4337
 {
@@ -12,6 +15,39 @@ namespace Group4337
             SecondWindow second = new SecondWindow();
             second.Show();
             this.Close();
+        }
+
+        private void ImportData_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                DefaultExt = "*.xls;*.xlsx",
+                Filter = "файл Excel (Spisok.xlsx)|*.xlsx",
+                Title = "Выберите файл базы данных"
+            };
+
+            if (!(openFileDialog.ShowDialog() == true))
+                return;
+
+            string[,] list;
+
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(openFileDialog.FileName);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+            
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            int _columns = (int)lastCell.Column;
+            int _rows = (int)lastCell.Row;
+
+            list = new string[_rows, _columns];
+
+            for (int j = 0; j < _columns; j++)
+                for (int i = 0; i < _rows; i++)
+                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text;
+
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+            ObjWorkExcel.Quit();
+            GC.Collect();
         }
     }
 }
