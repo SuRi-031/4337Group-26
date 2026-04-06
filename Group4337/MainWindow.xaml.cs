f3aed8820197ea19ce6ecfb4894cc3e5a451b5a0
Dilyara <di.su.re.07@gmail.com>
2026-04-06 09:26:10 +0300

    Закончила 3-ью лабу. Были прописаны обработчики нажатия кнопки "Импорт данных", а так же "Экспорт". Изменено главное, а так же второе окно.

--- Group4337/MainWindow.xaml.cs
+++ Group4337/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Win32;
+﻿using Group4337.Models;
+using Microsoft.Win32;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Windows;
 using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace Group4337
 {
@@ -50,6 +53,72 @@ namespace Group4337
             ObjWorkBook.Close(false, Type.Missing, Type.Missing);
             ObjWorkExcel.Quit();
             GC.Collect();
+
+            using (Isrpolab3Context dbContext = new Isrpolab3Context())
+            {
+                for (int i = 1; i < _rows; i++)
+                {
+                    dbContext.Users.Add(new User()
+                    {
+                        Post = list[i, 0], 
+                        UserLogin = list[i, 1]  
+                    });
+                }
+                dbContext.SaveChanges();
+            }
+
+            MessageBox.Show("Импорт завершён.", "Сообщение", MessageBoxButton.OK);
+        }
+
+        private void ExportData_Click(object sender, RoutedEventArgs e)
+        {
+            using (Isrpolab3Context dbContext = new Isrpolab3Context())
+            {
+                var allUsers = dbContext.Users.ToList();
+                var usersByPost = allUsers.GroupBy(u => u.Post).ToList();
+
+                Excel.Application excelApp = new Excel.Application();
+                excelApp.Visible = true;
+                Excel.Workbook workbook = excelApp.Workbooks.Add();
+
+                int sheetIndex = 1;
+
+                foreach (var postGroup in usersByPost)
+                {
+                    Excel.Worksheet worksheet;
+                    if (sheetIndex == 1)
+                        worksheet = (Excel.Worksheet)workbook.Sheets[sheetIndex];
+                    else
+                    {
+                        worksheet = (Excel.Worksheet)workbook.Sheets.Add();
+                        worksheet.Move(After: workbook.Sheets[workbook.Sheets.Count]);
+                    }
+
+                    worksheet.Name = postGroup.Key;
+
+                    worksheet.Cells[1, 1] = "ID";
+                    worksheet.Cells[1, 2] = "Должность";
+                    worksheet.Cells[1, 3] = "Логин пользователя";
+
+                    int row = 2;
+                    foreach (var user in postGroup)
+                    {
+                        worksheet.Cells[row, 1] = user.Id;
+                        worksheet.Cells[row, 2] = user.Post;
+                        worksheet.Cells[row, 3] = user.UserLogin;
+                        row++;
+                    }
+
+                    sheetIndex++;
+                }
+
+                if (usersByPost.Count == 0)
+                {
+                    Excel.Worksheet emptySheet = (Excel.Worksheet)workbook.Sheets[1];
+                    emptySheet.Name = "Нет данных";
+                    emptySheet.Cells[1, 1] = "Нет пользователей";
+                }
+            }
         }
     }
 }
